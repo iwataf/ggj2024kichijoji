43aee88936f9fbd9aec2921c646fe675f9ab9e66
Kazuya <86.climbing.1994@gmail.com>
2024-01-27 13:43:47 +0900

    StartScene、Faderの追加

--- MakeMeLaugh/Assets/Scripts/TitleCtrl.cs
+++ MakeMeLaugh/Assets/Scripts/TitleCtrl.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleCtrl : MonoBehaviour
 {
+    public void OnClickSampleScene()
+    {
+        Fader.Instance.FadeOut(SampleSceneLoad);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-		
+        Fader.Instance.FadeIn();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SampleSceneLoad()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
