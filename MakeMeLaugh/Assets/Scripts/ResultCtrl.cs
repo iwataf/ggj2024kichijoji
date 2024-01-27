182d01a42aa7928bd62dafe7d12c61175a93a6c4
Kazuya <86.climbing.1994@gmail.com>
2024-01-27 17:21:41 +0900

    result追加

---
+++ MakeMeLaugh/Assets/Scripts/ResultCtrl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultCtrl : MonoBehaviour
+{
+    [SerializeField] private ResultView _resultView;
+
+    private System.Action<bool> m_isClickCloseButton;
+
+    public void OnClickResultCloseButton()
+    {
+        m_isClickCloseButton.Invoke(true);
+    }
+
+    void Start()
+    {
+    }
+
+    public void OpenResult(int score, int maxValue, System.Action<bool> action)
+    {
+        _resultView.SetScoreText(score + "/" + maxValue);
+        gameObject.SetActive(true);
+        m_isClickCloseButton = action;
+    }
+
+}
