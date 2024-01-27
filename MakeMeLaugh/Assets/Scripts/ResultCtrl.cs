eda9208618a47e5170edd29317e7d9f4a2a640dc
Sotaro Kawabe <strkwb@gmail.com>
2024-01-27 19:22:40 +0900

    Resultの遷移アニメーションの追加

--- MakeMeLaugh/Assets/Scripts/ResultCtrl.cs
+++ MakeMeLaugh/Assets/Scripts/ResultCtrl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class ResultCtrl : MonoBehaviour
@@ -22,5 +23,13 @@ public class ResultCtrl : MonoBehaviour
         _resultView.SetScoreText(score + "/" + maxValue);
         gameObject.SetActive(true);
         m_isClickCloseButton = action;
+
+        // 遷移アニメーション
+        {
+            var pos = transform.position;
+            pos.x = -1920;
+            transform.position = pos;
+        }
+        transform.DOMoveX(0, 0.8f);
     }
 }
